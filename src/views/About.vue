2786508878f4eeae3f4df8dfa822f523780e937c
rubyon <rubyon@me.com>
2021-02-04 23:19:21 +0900

    axios 기본 사용법 (About.vue)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>
+      <a href="http://localhost:8080/api/getFoo">http://localhost:8080/api/getFoo</a> Json result:
+      {{ foo }}
+    </span>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'axios',
+  data() {
+    return {
+      foo: ''
+    }
+  },
+  mounted() {
+    const vm = this
+    axios
+      .get('http://localhost:8080/api/getFoo')
+      .then(function (response) {
+        vm.foo = response.data.foo
+      })
+      .catch(function (error) {
+        alert(error)
+      })
+  }
+}
+</script>
