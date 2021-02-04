6b1d383e3030ab4fe6e63f3110b6cd772ac5d709
rubyon <rubyon@me.com>
2021-02-04 22:56:06 +0900

    vuex store 사용법

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,37 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0,
+    greetings: 'vuex store sample'
+  },
+  mutations: {
+    increment(state) {
+      state.count += 1
+    },
+    decrement(state) {
+      state.count -= 1
+    }
+  },
+  actions: {
+    increment: ({ commit }) => commit('increment'),
+    decrement: ({ commit }) => commit('decrement'),
+    incrementIfOdd({ commit, state }) {
+      if ((state.count + 1) % 2 === 0) {
+        commit('increment')
+      }
+    },
+    incrementAsync({ commit }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          commit('increment')
+          resolve()
+        }, 1000)
+      })
+    }
+  },
+  getters: {
+    evenOrOdd: (state) => (state.count % 2 === 0 ? 'even' : 'odd')
+  },
   modules: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,29 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div>{{ greetings }}</div>
+  <div>{{ count }} : {{ evenOrOdd }}</div>
+  <button @click="increment">+</button>&nbsp;<button @click="decrement">-</button>&nbsp;
+  <button @click="incrementIfOdd">Increment if odd</button>&nbsp;
+  <button @click="incrementAsync">Increment async</button>
 </template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+    return {
+      greetings: store.state.greetings,
+      count: computed(() => store.state.count),
+      evenOrOdd: computed(() => store.getters.evenOrOdd),
+      increment: () => store.dispatch('increment'),
+      decrement: () => store.dispatch('decrement'),
+      incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+      incrementAsync: () => store.dispatch('incrementAsync')
+    }
+  }
+}
+</script>
